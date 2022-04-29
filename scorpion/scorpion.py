082ba898f31997f628de861c93d2574c07347db6
Brandom Moore <brandommoore@gmail.com>
2022-04-29 20:55:35 +0200

    triying with xmp data

--- scorpion/scorpion.py
+++ scorpion/scorpion.py
@@ -1,7 +1,10 @@
 import sys
+import os
 import exifread
 from PIL import Image
-from PIL.ExifTags import TAGS
+from PIL.ExifTags import TAGS, GPSTAGS
+from libxmp.utils import file_to_dict # Module for XMP metadata manipulation
+from libxmp import consts
 
 class bcolors:
 	RESET='\033[0m'
@@ -22,24 +25,35 @@ while i < len(sys.argv):
 	i+=1
 
 def extractEXIF(img):
-	print(bcolors.YELLOW + img + bcolors.GRAY + " data EXIF:\n" + bcolors.RESET)
-	imagen = Image.open(img)
-	exif_data = imagen.getexif()
-	exif_dic = {}
-	for tag_id, value in exif_data.items():
-		tag = TAGS.get(tag_id, tag_id)
-		valor = exif_data.get(tag_id)
-		exif_dic[tag] = valor
-		print(bcolors.TURQUOISE + str(tag) + " : " + bcolors.PURPLE + str(value) + bcolors.RESET)
+	try:
+		print(bcolors.YELLOW + img + bcolors.GRAY + " data EXIF:\n" + bcolors.RESET)
+		imagen = Image.open(img)
+		exif_data = imagen.getexif()
+		exif_dic = {}
+		for tag_id, value in exif_data.items():
+			tag = TAGS.get(tag_id, tag_id)
+			valor = exif_data.get(tag_id)
+			exif_dic[tag] = valor
+			print(bcolors.TURQUOISE + str(tag) + " : " + bcolors.PURPLE + str(value) + bcolors.RESET)
+	except Exception as e:
+		print(bcolors.RED + "Error\n" + bcolors.RESET)
+		pass
 
-def extractEXIF_nonjpg():
-	imagen = open("ejemplo.jpg", 'rb')
-	valores_exif = exifread.process_file(imagen)
-	for tag in valores_exif.keys():
-	    print(str(tag) + " : " + str(valores_exif[tag]))
+def extractEXIF_png_gif(img_name):
+	img = Image.open(img_name)
+	xmp_data = file_to_dict(img_name)
+	if not xmp_data:
+		print(bcolors.RED + "No metadata finded" + bcolors.RESET)
+	else:
+		data = xmp_data[consts.XMP_NS_DC]
+		print(data[0][0])
+		print(data[0][1])
+
+
 
 if __name__ == "__main__":
 
 	#print(bcolors.GREEN + "File Open Successfully!" + bcolors.RESET)
-	extractEXIF("Telos_Portada_118.jpeg")
+	extractEXIF("42-Barcelona-Conjunto.jpg")
+	# extractEXIF_nonjpg("bicimad.png")
 
