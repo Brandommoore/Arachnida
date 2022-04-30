6887f396152b1f483c9d6b432c2a304e8189e6e2
Brandom moore <brandommoore@gmail.com>
2022-05-01 01:13:39 +0200

    works absolutly perfect

--- scorpion/scorpion.py
+++ scorpion/scorpion.py
@@ -26,34 +26,65 @@ while i < len(sys.argv):
 
 def extractEXIF(img):
 	try:
-		print(bcolors.YELLOW + img + bcolors.GRAY + " data EXIF:\n" + bcolors.RESET)
 		imagen = Image.open(img)
 		exif_data = imagen.getexif()
-		exif_dic = {}
-		for tag_id, value in exif_data.items():
-			tag = TAGS.get(tag_id, tag_id)
-			valor = exif_data.get(tag_id)
-			exif_dic[tag] = valor
-			print(bcolors.TURQUOISE + str(tag) + " : " + bcolors.PURPLE + str(value) + bcolors.RESET)
+		if not exif_data:
+			print(bcolors.RED + "No metadata finded in " + bcolors.YELLOW + img + bcolors.RESET)
+		else:
+			print(bcolors.YELLOW + img + bcolors.GRAY + " data EXIF:\n" + bcolors.RESET)
+			exif_dic = {}
+			for tag_id, value in exif_data.items():
+				tag = TAGS.get(tag_id, tag_id)
+				valor = exif_data.get(tag_id)
+				exif_dic[tag] = valor
+				print(bcolors.TURQUOISE + str(tag) + " : " + bcolors.PURPLE + str(value) + bcolors.RESET)
+			print("\n")
+			img.close()
+	except Exception as e:
+		#print(bcolors.RED + "Error\n" + bcolors.RESET)
+		pass
+
+def extractEXIF_png_gif(img_name):
+	try:
+		img = Image.open(img_name)
+		xmp_data = file_to_dict(img_name)
+		if not xmp_data:
+			print(bcolors.RED + "No metadata finded in " + bcolors.YELLOW + img_name + bcolors.RESET)
+		else:
+			print(bcolors.YELLOW + img_name + " data XMP:\n" + bcolors.RESET)
+			data = xmp_data[consts.XMP_NS_DC]
+			print(bcolors.GREEN + data[0][0] + bcolors.RESET)
+			print(bcolors.GREEN + data[0][1] + bcolors.RESET)
+			for key, value in data[0][2].items():
+				print(bcolors.TURQUOISE + str(key) + " : " + bcolors.PURPLE + str(value) + bcolors.RESET)
+			print("\n")
+		img.close()
 	except Exception as e:
 		print(bcolors.RED + "Error\n" + bcolors.RESET)
 		pass
 
-def extractEXIF_png_gif(img_name):
-	img = Image.open(img_name)
-	xmp_data = file_to_dict(img_name)
-	if not xmp_data:
-		print(bcolors.RED + "No metadata finded" + bcolors.RESET)
+def scorpionEXTRACTER(img_name):
+	if img_name.endswith((".jpg", ".jpeg")):
+		#print("IS JPG")
+		extractEXIF(img_name)
+	elif img_name.endswith((".png", ".gif", ".bpm")):
+		#print("NO JPG")
+		extractEXIF_png_gif(img_name)
 	else:
-		data = xmp_data[consts.XMP_NS_DC]
-		print(data[0][0])
-		print(data[0][1])
+		print(bcolors.RED + "File extension not supported\n" + bcolors.RESET)
 
+def cleanProgName(arguments):
+	progName=arguments[0]
+	for arg in arguments:
+		if arg is progName:
+			arguments.remove(arg)
+	return(arguments)
 
 
 if __name__ == "__main__":
-
-	#print(bcolors.GREEN + "File Open Successfully!" + bcolors.RESET)
-	extractEXIF("42-Barcelona-Conjunto.jpg")
-	# extractEXIF_nonjpg("bicimad.png")
+	arguments=[]
+	arguments=sys.argv
+	arguments=cleanProgName(arguments)
+	for img in arguments:
+		scorpionEXTRACTER(img)
 
